9dc1ff6df1e3dcaaa07bdfdea13fc9973fa80afc
NightmareVCO <vladimircuriel@outlook.com>
2025-09-09 17:59:58 -0400

    feat: add new SVG icons for actions, file management, and user interactions

---
+++ src/components/icons/GridIcon.tsx
@@ -0,0 +1,21 @@
+export default function GridIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-grid2x2-icon lucide-grid-2x2 size-4"
+    >
+      <title>Grid Icon</title>
+      <path d="M12 3v18" />
+      <path d="M3 12h18" />
+      <rect x="3" y="3" width="18" height="18" rx="2" />
+    </svg>
+  )
+}
---
+++ src/components/icons/TextIcon.tsx
@@ -0,0 +1,21 @@
+export default function TextIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="0.7"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-file-text-icon lucide-file-text size-46"
+    >
+      <title>File Text Icon</title>
+      <path d="M21 5H3" />
+      <path d="M15 12H3" />
+      <path d="M17 19H3" />
+    </svg>
+  )
+}
