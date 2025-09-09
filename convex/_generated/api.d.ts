e18a7cc7521e6785553bdce856382e7bed7d2b64
NightmareVCO <vladimircuriel@outlook.com>
2025-09-09 02:17:31 -0400

    feat: enhance API and data model types with additional modules and update package dependencies

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,11 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as clerk from "../clerk.js";
+import type * as crons from "../crons.js";
+import type * as files from "../files.js";
+import type * as http from "../http.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -22,7 +27,13 @@ import type {
  * const myFunctionReference = api.myModule.myFunction;
  * ```
  */
-declare const fullApi: ApiFromModules<{}>;
+declare const fullApi: ApiFromModules<{
+  clerk: typeof clerk;
+  crons: typeof crons;
+  files: typeof files;
+  http: typeof http;
+  users: typeof users;
+}>;
 export declare const api: FilterApi<
   typeof fullApi,
   FunctionReference<any, "public">
