47cc97185ecee4a57f95f5315e473d092664418b
NightmareVCO <vladimircuriel@outlook.com>
2025-09-09 01:18:27 -0400

    feat: add Background, CloudNextIcon, CloudNextLogo, and RightIcon components

---
+++ src/components/brand/CloudNextIcon.tsx
@@ -0,0 +1,90 @@
+export default function CloudNextIcon() {
+  return (
+    <svg
+      width={54}
+      height={54}
+      xmlns="http://www.w3.org/2000/svg"
+      fillRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit="2"
+      clipRule="evenodd"
+      viewBox="0 0 20 20"
+    >
+      <title>CloudNext Icon</title>
+      <path
+        fill="url(#a)"
+        d="m 7,2 h 5.18 c 0.79,0 1.548,0.314 2.107,0.872 0.53,0.531 1.31,1.311 1.841,1.841 C 16.686,5.272 17,6.03 17,6.82 v 8.201 c 0,0.79 -0.314,1.548 -0.872,2.107 C 15.569,17.686 14.811,18 14.021,18 11.796,18 8.204,18 5.979,18 5.189,18 4.431,17.686 3.872,17.128 3.314,16.569 3,15.811 3,15.021 V 6 C 3,5.448 3.448,5 4,5 5.104,5 6,4.104 6,3 6,2.448 6.448,2 7,2 Z M 5,3 C 5,3.552 4.552,4 4,4 3.448,4 3,3.552 3,3 3,2.448 3.448,2 4,2 4.552,2 5,2.448 5,3 Z"
+      />
+      <path
+        fill="#4054d4"
+        d="m 7.518,15 h 4 c 0.552,0 1,-0.448 1,-1 0,-0.552 -0.448,-1 -1,-1 h -4 c -0.552,0 -1,0.448 -1,1 0,0.552 0.448,1 1,1 z m 0,-3 h 2 c 0.552,0 1,-0.448 1,-1 0,-0.552 -0.448,-1 -1,-1 h -2 c -0.552,0 -1,0.448 -1,1 0,0.552 0.448,1 1,1 z"
+      />
+      <path
+        fill="#5d6ed5"
+        d="m 9.518,4 v 3.036 c 0,1.637 1.327,2.964 2.964,2.964 0.552,0 1,-0.448 1,-1 0,-0.552 -0.448,-1 -1,-1 -0.532,0 -0.964,-0.432 -0.964,-0.964 0,0 0,-3.036 0,-3.036 0,-0.552 -0.448,-1 -1,-1 -0.552,0 -1,0.448 -1,1 z"
+      />
+      <path
+        fill="url(#b)"
+        d="M 10,3 V 6.03625 C 10,7.6725 11.3275,9 12.96375,9 c 0.5525,0 1,-0.4475 1,-1 0,-0.5525 -0.4475,-1 -1,-1 C 12.43125,7 12,6.56875 12,6.03625 12,6.03625 12,3 12,3 12,2.4475 11.5525,2 11,2 10.4475,2 10,2.4475 10,3 Z"
+      />
+      <path
+        fill="url(#c)"
+        d="m 8,11 h 2 c 0.552,0 1,-0.448 1,-1 C 11,9.448 10.552,9 10,9 H 8 c -0.552,0 -1,0.448 -1,1 0,0.552 0.448,1 1,1 z"
+      />
+      <path
+        fill="url(#d)"
+        d="m 8,14 h 4 c 0.552,0 1,-0.448 1,-1 0,-0.552 -0.448,-1 -1,-1 H 8 c -0.552,0 -1,0.448 -1,1 0,0.552 0.448,1 1,1 z"
+      />
+      <defs>
+        <linearGradient
+          id="a"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+          gradientTransform="matrix(12 -15 15 12 4 18)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0" stopColor="#2945fa" />
+          <stop offset="1" stopColor="#98a7ff" />
+        </linearGradient>
+        <linearGradient
+          id="b"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+          gradientTransform="matrix(3 -6 6 3 10 9)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0" stopColor="#98a7ff" />
+          <stop offset="1" stopColor="#fff" />
+        </linearGradient>
+        <linearGradient
+          id="c"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+          gradientTransform="matrix(4 -2 2 4 7 11)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0" stopColor="#98a7ff" />
+          <stop offset="1" stopColor="#fff" />
+        </linearGradient>
+        <linearGradient
+          id="d"
+          x1="0"
+          x2="1"
+          y1="0"
+          y2="0"
+          gradientTransform="matrix(6 -2 2 6 7 14)"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0" stopColor="#98a7ff" />
+          <stop offset="1" stopColor="#fff" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
---
+++ src/components/icons/RightIcon.tsx
@@ -0,0 +1,15 @@
+export default function RightIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <title>Right Arrow Icon</title>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+    </svg>
+  )
+}
